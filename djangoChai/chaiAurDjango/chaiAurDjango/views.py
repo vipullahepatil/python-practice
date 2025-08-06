7d61608c36a0fe18b26f5357c4eac8bdfa3c51c8
Vipul Lahe <vipullhepatil@gmail.com>
2025-08-06 18:50:34 +0000

    template routing

--- djangoChai/chaiAurDjango/chaiAurDjango/views.py
+++ djangoChai/chaiAurDjango/chaiAurDjango/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     # return HttpResponse("Hello, Worlds. You are at chai aur Django Home Page")
-    return render(request, "index.html")
+    return render(request, "website/index.html")
 
 def about(request):
     return HttpResponse("Hello, Worlds. You are at chai aur Django About Page")
