a1e690222492060567a21e6d2139db3570cce7c6
Vipul Lahe <vipullhepatil@gmail.com>
2025-08-06 18:38:24 +0000

    template add

--- djangoChai/chaiAurDjango/chaiAurDjango/views.py
+++ djangoChai/chaiAurDjango/chaiAurDjango/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Hello, Worlds. You are at chai aur Django Home Page")
+    # return HttpResponse("Hello, Worlds. You are at chai aur Django Home Page")
+    return render(request, "index.html")
 
 def about(request):
     return HttpResponse("Hello, Worlds. You are at chai aur Django About Page")
