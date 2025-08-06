5bde8d1e29153f7081ed30d43dcfdd8f49c1abe9
Vipul Lahe <vipullhepatil@gmail.com>
2025-08-06 19:20:45 +0000

    assignment compate ,  htmls page add nav footer

--- djangoChai/chaiAurDjango/chaiAurDjango/views.py
+++ djangoChai/chaiAurDjango/chaiAurDjango/views.py
@@ -6,8 +6,10 @@ def home(request):
     return render(request, "website/index.html")
 
 def about(request):
-    return HttpResponse("Hello, Worlds. You are at chai aur Django About Page")
+    # return HttpResponse("Hello, Worlds. You are at chai aur Django About Page")
+    return render(request, "website/about.html")
 
 def contact(request):
-    return HttpResponse("Hello, Worlds. You are at chai aur Django Contact Page")
+    # return HttpResponse("Hello, Worlds. You are at chai aur Django Contact Page")
+    return render(request, "website/contact.html")
 
